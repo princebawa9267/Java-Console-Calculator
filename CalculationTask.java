6b2bda972f19b13f58680f5fcfd3f95f28833f94
princebawa9267 <136613646+princebawa9267@users.noreply.github.com>
2025-09-22 14:02:37 +0530

    Console Calculator complete

---
+++ CalculationTask.java
@@ -0,0 +1,11 @@
+
+// This is used to implement oops concepts 
+
+public class CalculationTask {
+    double add(double a, double b) { return a+b; }
+    double subtraction(double a, double b) { return a-b; }
+    double multiplication(double a, double b) { return a*b; }
+    double modulus(double a, double b) { return a%b; }
+    double division(double a, double b) { return a/b; }
+    double power(double a, double b){ return Math.pow(a, b);}
+}
---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n\n\n------------------------ Welcome to Calculator ------------------------\n");
+        CalculationTask calculationTask = new CalculationTask();
+
+        while (true) {
+            System.out.println("\n\n------------- Choose from these options 1-8 -------------\n");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Subtraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Modulus (%)");
+            System.out.println("6. Power (a^b)");
+            System.out.println("7. Square Root ");
+            System.out.println("8. Exit");
+
+            int operation = scan.nextInt();
+
+            if (operation == 8) {
+                System.out.println("Thanks!! for using this calculator ");
+                break;
+            }
+
+            else if (operation == 7) {
+                System.out.print("Enter number: ");
+                double num = scan.nextDouble();
+                if (num < 0) System.out.println("For square root number can't be negative!!!");
+                else System.out.printf("Square root of %.2f is %.2f\n", num, Math.sqrt(num));
+            } 
+            
+            else {
+                System.out.print("Enter first number: ");
+                double num1 = scan.nextDouble();
+                System.out.print("Enter second number: ");
+                double num2 = scan.nextDouble();
+
+                switch (operation) {
+                    case 1 -> System.out.printf("%.2f + %.2f = %.2f \n", num1, num2, calculationTask.add(num1, num2));
+                    case 2 -> System.out.printf("%.2f - %.2f = %.2f \n", num1, num2, calculationTask.subtraction(num1, num2));
+                    case 3 -> System.out.printf("%.2f * %.2f = %.2f \n", num1, num2, calculationTask.multiplication(num1, num2));
+                    case 4 -> System.out.printf("%.2f / %.2f = %.2f \n", num1, num2, calculationTask.division(num1, num2));
+                    case 5 -> System.out.printf("%.2f %% %.2f = %.2f \n", num1, num2, calculationTask.modulus(num1, num2));
+                    case 6 -> System.out.printf("%.2f ^ %.2f = %.2f \n", num1, num2, calculationTask.power(num1, num2));
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+        }
+        scan.close();
+    }
+}
